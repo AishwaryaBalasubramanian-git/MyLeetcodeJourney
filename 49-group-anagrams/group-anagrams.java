e495aa3838731196efbeea810bf68f4fca4911f3
AishwaryaBalasubramanian-git <aishwaryab1804@gmail.com>
2025-01-19 23:34:29 +0530

    Time: 6 ms (98.02%) | Memory: 47.8 MB (78.69%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,19 @@
+class Solution {
+    //myleetcodejourney
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs.length==0) return new ArrayList();
+        HashMap<String, List> map = new HashMap<>();
+        for(String s : strs){
+            char[] array = s.toCharArray();
+            Arrays.sort(array);
+            String key =String.valueOf(array);
+            if(!map.containsKey(key)){
+                map.put(key,new ArrayList());
+            }
+            map.get(key).add(s);
+        }
+        return new ArrayList(map.values());
+
+        
+    }
+}
